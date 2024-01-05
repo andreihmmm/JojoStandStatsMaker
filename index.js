7dd96d3d659deca844dbb029e6230dddb9309069
Andrei Alves Fracalossi <124471216+andreihmmm@users.noreply.github.com>
2024-01-05 00:20:18 -0300

    Botão pra salvar o atual feito
    cores mudadas, novo chart, stands salvos de cima para baixo...

--- index.js
+++ index.js
@@ -29,3 +29,22 @@ function botaoStats() {
             break;
     }
 }
+
+function salvarAtual() {
+    var nomeInput = document.querySelector("#input-nome").value;
+    const lista = document.querySelector("body > header > ul");
+    var novoLi = document.createElement('li');
+    novoLi.classList.add('salvo');
+    lista.appendChild(novoLi);
+    var a = document.createElement('a');
+    var nomeOutput = document.createElement('p');
+    novoLi.appendChild(a);
+    a.appendChild(nomeOutput);
+    nomeOutput.append(nomeInput);
+
+    var imagem = document.createElement('img');
+    imagem.classList.add('img-salvos');
+    imagem.src = "https://images-wixmp-ed30a86b8c4ca887773594c2.wixmp.com/f/dda829fd-4943-49b6-8569-41bf58368248/dcf30ck-d5732a87-b415-4c42-b74b-c2b8ed8a08a8.jpg/v1/fill/w_681,h_750,q_75,strp/jojo_s_bizarre_adventure_stand_stat_chart_by_optimusomega_dcf30ck-fullview.jpg?token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ1cm46YXBwOjdlMGQxODg5ODIyNjQzNzNhNWYwZDQxNWVhMGQyNmUwIiwiaXNzIjoidXJuOmFwcDo3ZTBkMTg4OTgyMjY0MzczYTVmMGQ0MTVlYTBkMjZlMCIsIm9iaiI6W1t7ImhlaWdodCI6Ijw9NzUwIiwicGF0aCI6IlwvZlwvZGRhODI5ZmQtNDk0My00OWI2LTg1NjktNDFiZjU4MzY4MjQ4XC9kY2YzMGNrLWQ1NzMyYTg3LWI0MTUtNGM0Mi1iNzRiLWMyYjhlZDhhMDhhOC5qcGciLCJ3aWR0aCI6Ijw9NjgxIn1dXSwiYXVkIjpbInVybjpzZXJ2aWNlOmltYWdlLm9wZXJhdGlvbnMiXX0.qwPRotoCOPklehwcgFQkRzV-pT9q63I8o4m6M29jmDg"
+    novoLi.appendChild(imagem);
+    lista.scrollTop = lista.scrollHeight
+}
